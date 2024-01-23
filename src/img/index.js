da60e5d45760d784bd4e919c608318c4788214c8
andres perez <andres.perez.jktic@gmail.com>
2024-01-23 17:17:36 -0500

    Tarjeta v.1 : Img: Se agregan imagenes usadas en la foto de la tieneda y se estandarizan la forma de usar las imagenes

---
+++ src/img/index.js
@@ -0,0 +1,7 @@
+import chat from './chat.png'
+import like from './heart.png'
+import tienda from './negocio_local.jpeg'
+import logo from './logo.png'
+import enviar from './send.png'
+
+export {chat, like, tienda, logo, enviar}
