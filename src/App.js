ad096c6e269ffff3483c46e06643133e3412a7c8
andres perez <andres.perez.jktic@gmail.com>
2024-01-23 17:20:46 -0500

    Tarjeta v.1 : App.js : se agrega el componente de servicios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Bannner from './components/Banner/Bannner'
+import Servicios from './components/Servicios/Servicios'
+
 
 const App = () => {
   return (
     <div>
       <Bannner/>
+      <Servicios/>
     </div>
   )
 }
