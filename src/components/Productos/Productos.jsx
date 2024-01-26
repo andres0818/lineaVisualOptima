dc0abbad66a3d854709222264e8a3d6eb10a70d8
andres perez <andres.perez.jktic@gmail.com>
2024-01-25 19:02:57 -0500

    Tarjeta v.1 : finalizacion proyecto

--- src/components/Productos/Productos.jsx
+++ src/components/Productos/Productos.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { montura } from "../../img";
 import './Productos.scss'
+import video from '../video/infantil.mp4'
 
 export const Productos = () => {
   return (
@@ -9,7 +10,12 @@ export const Productos = () => {
       <div className="card_container">
         <img className="producto_card" src={montura} alt="" />
         <img className="producto_card" src={montura} alt="" />
-        <img className="producto_card" src={montura} alt="" />
+
+        <video className="producto_card" muted autoPlay loop>
+        <source src={video} type="video/mp4" />
+        Tu navegador no soporta el tag de video.
+      </video>
+
         <iframe className="producto_card"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.528725614731!2d-75.56685903040454!3d6.2485867327462765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e4428f88321d515%3A0x181f3dd595daf571!2sCra.%2047%20%2349-89%2C%20La%20Candelaria%2C%20Medell%C3%ADn%2C%20La%20Candelaria%2C%20Medell%C3%ADn%2C%20Antioquia!5e0!3m2!1ses!2sco!4v1706050559263!5m2!1ses!2sco"
           allowfullscreen=""
--- src/img/index.js
+++ src/img/index.js
@@ -4,5 +4,7 @@ import tienda from './negocio_local.jpeg'
 import logo from './logo.png'
 import enviar from './send.png'
 import montura from './montura.jpeg'
+import logo_azul from './logo_azul.png'
+import logo_azul_horizontal from './logo_azul_horizontal.png'
 
-export {chat, like, tienda, logo, enviar, montura}
+export {chat, like, tienda, logo, enviar, montura, logo_azul, logo_azul_horizontal}
