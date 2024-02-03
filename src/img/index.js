1a28364255d290356e05386225519fde936e9ef2
andres perez <andres.perez.jktic@gmail.com>
2024-02-03 14:23:16 -0500

    fixed bugs

--- src/img/index.js
+++ src/img/index.js
@@ -6,5 +6,8 @@ import enviar from './send.png'
 import montura from './montura.jpeg'
 import logo_azul from './logo_azul.png'
 import logo_azul_horizontal from './logo_azul_horizontal.png'
+import QrInstagram from './LineaVisualOptica.svg'
+import whatsApp from './whatsapp.png'
+import instagram from './instagram.png'
 
-export {chat, like, tienda, logo, enviar, montura, logo_azul, logo_azul_horizontal}
+export {chat, like, tienda, logo, enviar, montura, logo_azul, logo_azul_horizontal, QrInstagram, whatsApp, instagram}
